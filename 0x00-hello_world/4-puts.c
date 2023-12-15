4bc46477d7ba6acb37dd3da169f7906af40e5dea
Kamogelo Loate <kamogeloloate@gmail.com>
2023-12-14 16:45:09 -0800

    Completed Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*why does not adding int next to main as well as void as a main argument make my program bomb*/
+
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle.");
+  return(0);
+
+}
