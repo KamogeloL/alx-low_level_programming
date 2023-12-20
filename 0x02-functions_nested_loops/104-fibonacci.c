5c7a7ca82207c73a84babd80bbdd51f43687effc
Kamogelo Loate <kamogeloloate@gmail.com>
2023-12-20 14:58:15 -0800

    Correct update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Computes and prints the sum of even-valued terms in the Fibonacci sequence below 4,000,000.
- *
+ * main - prints the first 98 Fibonacci numbers separated by commas
  * Return: Always 0.
  */
 int main(void)
 {
-    const unsigned long n = 4000000;
-    unsigned long num_1, num_2, num_3;
-    unsigned long result;
+unsigned long fib1 = 1, fib2 = 2, next_term;
+int count;
 
-    result = 0;
-    num_1 = 1;
-    num_2 = 2;
+printf("%lu, %lu", fib1, fib2); // Print the first two Fibonacci numbers
 
-    while (num_1 <= n)
-    {
-        if (num_1 % 2 == 0)
-        {
-            result += num_1;
-        }
-        num_3 = num_2;
-        num_2 = num_1 + num_2;
-        num_1 = num_3;
-    }
+for (count = 3; count <= 98; ++count)
+{
+next_term = fib1 + fib2;
 
-    printf("%lu\n", result);
-    return (0);
+printf(", %lu", next_term);
+
+fib1 = fib2;
+fib2 = next_term;
 }
+
+printf("\n");
+return (0);
+}
+
