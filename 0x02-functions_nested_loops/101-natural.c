d59167adca4fe8a8b86a9190734574ff851a839d
Kamogelo Loate <kamogeloloate@gmail.com>
2023-12-20 14:44:28 -0800

    Batch commit advanced optional tasks

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of multiples of 3 or 5 below 1024
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i, result; /* Declaring statements */
+
+i = 0;
+result = 0;
+/* Start While */
+while (i < 1024)
+{
+if (i % 3 == 0 || i % 5 == 0) /* Start condition IF */
+{
+result += i;
+} /* End condition IF */
+i++;
+} /* END While */
+
+printf("%d\n", result);
+    return (0);
+}
+
