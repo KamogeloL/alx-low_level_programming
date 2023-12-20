9aa2e6a2453cb1d5baa185601059b2b9ef7f268b
Kamogelo Loate <kamogeloloate@gmail.com>
2023-12-20 15:59:00 -0800

    Final commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,32 +6,33 @@
  */
 int main(void)
 {
-unsigned long first, second, next, i;
+long int first, second, next, i;
+
 
 /*Initialize the first two terms of the Fibonacci sequence*/
 first = 1;
 second = 2;
 
 /*Print the initial values*/
-printf("%lu, %lu, ", first, second);
+printf("%ld, %ld, ", first, second);
 
 /*Generate and print the Fibonacci sequence up to the 98th term*/
 for (i = 3; i <= 98; i++)
 {
-/*Calculate the next term in the sequence*/ 
+/*Calculate the next term in the sequence*/
 next = first + second;
 
-/*Print the current term with appropriate formatting*/ 
+/*Print the current term with appropriate formatting*/
 if (i == 98)
 {
-printf("%lu", next);
+printf("%ld", next);
 }
 else
 {
-printf("%lu, ", next);
+printf("%ld, ", next);
 }
 
-/*Update values for the next iteration*/ 
+/*Update values for the next iteration*/
 first = second;
 second = next;
 }
